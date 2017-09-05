879aecfc1852f2b2ad5abcb6fb2a4ab46ceb7de3
Juri <juri.l@me.com>
2017-09-05 11:47:20 +0300

    Update table-data.js

--- src/reducers/table-data.js
+++ src/reducers/table-data.js
@@ -1,1 +1,9 @@
-import 
+import { READ_SOURCE_FILE_TREE } from '../actions/index';
+
+export default function TableData(state={}, action) {
+  switch (action.type) {
+    case READ_SOURCE_FILE_TREE:
+      return action.payload;
+  }
+  return state;
+}  
