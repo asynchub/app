ef0817413989f00d056470a4bc84443fa776d59d
Juri <juri.l@me.com>
2017-09-05 11:46:06 +0300

    Update read-source-file-tree.js

--- src/actions/read-source-file-tree.js
+++ src/actions/read-source-file-tree.js
@@ -2,9 +2,10 @@ export const READ_SOURCE_FILE_TREE = 'READ_SOURCE_FILE_TREE';
 
 export function readSourceFileTree(files) {
   // fetch data from documents
+  // write data into array or tableData object
   // pass data to reducer as payload
   return {
     type: READ_SOURCE_FILE_TREE,
-    payload: data
+    payload: tableData
   };
 }
