e37dba0371b8a3d66ca1da4b78ff9702f6a582d0
Juri <juri.l@me.com>
2017-09-07 11:49:13 +0300

    Update state_data.js

--- src/state_data.js
+++ src/state_data.js
@@ -15,19 +15,29 @@ productTree {
         title : "SPS-00-000",
         description : "Main Assembly",
         material : "",
-        qty : "2",
-        assemblies : [ "assembly1" ],
+        qtyInAss : "2",
+        assemblies : [ "assembly2", "assembly3" ],
         details : []
       }
       "assembly2" : {
         id :"assembly2",
         title : "SPS-10-000",
-        description : "Main Assembly",
+        description : "Sub Assembly 10",
         material : "",
-        qty : "2",
+        qtyInAss : "4",
+        assemblies : [],
+        details : []
+      }
+      "assembly3" : {
+        id :"assembly3",
+        title : "SPS-20-000",
+        description : "Sub Assembly 20",
+        material : "",
+        qtyInAss : "4",
         assemblies : [],
         details : []
       }
     }
+    allIds: [ "assembly1", "assembly2", "assembly3" ]
   }
 }
