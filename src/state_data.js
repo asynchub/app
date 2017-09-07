c741281e918c7dd31b2a21ff02f07bc1f22a027d
Juri <juri.l@me.com>
2017-09-07 11:41:26 +0300

    Create state_data.js

---
+++ src/state_data.js
@@ -0,0 +1,33 @@
+organizer {}
+products {}
+resources {}
+stocks {}
+materials {}
+customers {}
+counter {}
+sourceFileTree {}
+syncFileTree {}
+productTree {
+  assemblies : {
+    byId : {
+      "assembly1" : {
+        id :"assembly1",
+        title : "SPS-00-000",
+        description : "Main Assembly",
+        material : "",
+        qty : "2",
+        assemblies : [ "assembly1" ],
+        details : []
+      }
+      "assembly2" : {
+        id :"assembly2",
+        title : "SPS-10-000",
+        description : "Main Assembly",
+        material : "",
+        qty : "2",
+        assemblies : [],
+        details : []
+      }
+    }
+  }
+}
