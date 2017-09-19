1e626b8c26af625e564c6cc48fdb71520b964678
Juri <juri.l@me.com>
2017-09-19 12:46:08 +0300

    Update reducer-parts.js

--- src/reducers/reducer-parts.js
+++ src/reducers/reducer-parts.js
@@ -1,23 +1,34 @@
 import { combineReducers } from 'redux';
+import { 
+  EDIT_CELL,
+  ADD_PART
+} from '../actions/index';
 
 function editPartProperty(state={}, action) {
-  
+  const { payload } = action;
+  const { partId, partProperty, propertyValue } = payload;
+  const part = { ...state[partId], partProperty: propertyValue };
+  return { ...state, partId: part };
+}
+
+function addPart(state={}, action) {
   
 }
 
 function partsById(state={}, action) {
   switch (action.payload) {
     case EDIT_CELL: return editPartProperty(state, action);
+    case ADD_PART: return addPart(state, action);
     default: return state;
   }    
 }
-  
+
 
 function allIds(state={}, action) {
   
 }
 
-function partsReducer = combineReducers({
+export default function partsReducer = combineReducers({
   byId: partsById,
   allIds: allParts,
 })
