6842676ba73aac384031c223125557bf5a65cac4
Juri <juri.l@me.com>
2017-09-29 13:56:23 +0300

    Update reducer-materials.js

--- src/reducers/reducer-materials.js
+++ src/reducers/reducer-materials.js
@@ -1,8 +1,116 @@
 import { combineReducers } from 'redux';
 
 import {
-  FETCH_MATERIALS_FROM_DB
+  FETCH_MATERIALS_FROM_DB, // to hydrate all the slices of this state, besides stockMaterials slices
+  // to hydrate stockMaterial slices of this state with 
+  // materials according to 3 material criterias in a row: grade, profile, size
+  FETCH_STOCK_MATERIALS_FROM_DB, 
+  
 }
+
+
+
+
+function gradesByGradeStd(state={}, action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+function allGradesStds(state=[], action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+function allGrades(state=[], action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+
+
+function sizesByProfile(state={}, action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+function allProfiles(state=[], action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+function allSizes(state=[], action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+
+
+function profilesByToleranceStds(state={}, action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+function allToleranceStds(state=[], action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+
+
+function surfaceStdsByProfiles(state={}, action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+function allSurfaceStds(state=[], action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+
+
+function surfaceClassesBySurfaceStds(state={}, action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+function allSurfaceClasses(state=[], action) {
+  switch(action.type) {
+    default: return state;
+  }
+}
+
+
+
+function stockMaterialsById(state={}, action) {
+  const { payload } = action;
+  switch(action.type) {
+    case: FETCH_STOCK_MATERIALS_FROM_DB: return payload.stockMaterialsById;
+    default: return state;
+  }
+}
+
+function stockMaterialsAllIds(state=[], action) {
+  const { payload } = action;
+  switch(action.type) {
+    case: FETCH_STOCK_MATERIALS_FROM_DB: return payload.stockMaterialsAllIds;
+    default: return state;
+  }
+}
+
+
+
 
 const materialsReducer = combineReducers({
   gradesByGradeStd, // grades grouped into standards accordingly
@@ -23,7 +131,7 @@ const materialsReducer = combineReducers({
   allSurfaceClasses, // array of all surface classes
   
   stockMaterialsById, // object of stock materials, fetched according some or all criterias above in row of cell being edited
-  allStockMaterialsIds, // array of all stock materials, fetched according some or all criterias above in row of cell being edited
+  stockMaterialsAllIds, // array of all stock materials, fetched according some or all criterias above in row of cell being edited
 });
 
 export default materialsReducer;
