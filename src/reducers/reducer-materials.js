b356227febae8d5f71c0527d3635a1a450ad3a80
Juri <juri.l@me.com>
2017-09-29 19:18:30 +0300

    Update reducer-materials.js

--- src/reducers/reducer-materials.js
+++ src/reducers/reducer-materials.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 
 import {
   FETCH_MATERIALS_FROM_DB, // to hydrate all the slices of this state, besides stockMaterials slices
-  // to hydrate stockMaterial slices of this state with 
+  // to hydrate stockMaterial slices of this state with
   // materials according to 3 material criterias in a row: grade, profile, size
-  FETCH_STOCK_MATERIALS_FROM_DB, 
-  FILTER_OPTIONS_BY,
+  FETCH_STOCK_MATERIALS_FROM_DB,
+  FILTER_OPTIONS_BY_GRADE_STDS,
+  FILTER_OPTIONS_BY_PROFILES,
+  FILTER_OPTIONS_BY_TOLERANCE_STDS,
+  FILTER_OPTIONS_BY_SURFACE_STDS,
   FILTER_STOCK_MATERIALS_BY
 }
 
@@ -13,15 +16,16 @@ import {
 // FILTER_OPTIONS_BY action.payload will have both: objects and arrays to return new object here
 function filterObject(state={}, action) {
   const { payload } = action;
-  // return new state without arrays by id's that are not passed by action
-  return state; 
+  // return new state without arrays by id's that are not passed by action payload
+  return state;
 }
 
 // this is general helper function, called from array slice reducers on action FILTER_OPTIONS_BY
 // FILTER_OPTIONS_BY action.payload will have both: objects and arrays to return new array here
+// in jsx implement autofill cell value if the length of list is 1 after filterList
 function filterList(state=[], action) {
   const { payload } = action;
-  // return new state without arrays by id's that are not passed by action
+  // return new state without arrays by id's that are not passed by action payload
   return state;
 }
 
@@ -30,7 +34,7 @@ function gradesByGradeStd(state={}, action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.gradesByGradeStd;
-    case FILTER_OPTIONS_BY_GRADE: return filterObject(state, action);
+    case FILTER_OPTIONS_BY_GRADE_STDS: return filterObject(state, action);
     default: return state;
   }
 }
@@ -39,8 +43,7 @@ function allGradesStds(state=[], action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.allGradesStds;
-    case FILTER_OPTIONS_BY_GRADE: return filterList(state, action);
-    (state, action);
+    case FILTER_OPTIONS_BY_GRADE_STDS: return filterList(state, action);
     default: return state;
   }
 }
@@ -49,7 +52,7 @@ function allGrades(state=[], action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.allGrades;
-    case FILTER_OPTIONS_BY_GRADE: return filterList(state, action);
+    case FILTER_OPTIONS_BY_GRADE_STDS: return filterList(state, action);
     default: return state;
   }
 }
@@ -60,7 +63,7 @@ function sizesByProfile(state={}, action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.sizesByProfile;
-    case FILTER_OPTIONS_BY: return filterObject(state, action);
+    case FILTER_OPTIONS_BY_PROFILES: return filterObject(state, action);
     default: return state;
   }
 }
@@ -69,7 +72,7 @@ function allProfiles(state=[], action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.allProfiles;
-    case FILTER_OPTIONS_BY: return filterList(state, action);
+    case FILTER_OPTIONS_BY_PROFILES: return filterList(state, action);
     default: return state;
   }
 }
@@ -78,7 +81,7 @@ function allSizes(state=[], action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.allProfiles;
-    case FILTER_OPTIONS_BY: return filterList(state, action);
+    case FILTER_OPTIONS_BY_PROFILES: return filterList(state, action);
     default: return state;
   }
 }
@@ -89,7 +92,7 @@ function profilesByToleranceStds(state={}, action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.profilesByToleranceStds;
-    case FILTER_OPTIONS_BY: return filterObject(state, action);
+    case FILTER_OPTIONS_BY_TOLERANCE_STDS: return filterObject(state, action);
     default: return state;
   }
 }
@@ -98,7 +101,7 @@ function allToleranceStds(state=[], action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.allToleranceStds;
-    case FILTER_OPTIONS_BY: return filterList(state, action);
+    case FILTER_OPTIONS_BY_TOLERANCE_STDS: return filterList(state, action);
     default: return state;
   }
 }
@@ -109,7 +112,7 @@ function surfaceStdsByProfiles(state={}, action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.surfaceStdsByProfiles;
-    case FILTER_OPTIONS_BY: return filterObject(state, action);
+    case FILTER_OPTIONS_BY_PROFILES: return filterObject(state, action);
     default: return state;
   }
 }
@@ -118,7 +121,7 @@ function allSurfaceStds(state=[], action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.allSurfaceStds;
-    case FILTER_OPTIONS_BY: return filterList(state, action);
+    case FILTER_OPTIONS_BY_PROFILES: return filterList(state, action);
     default: return state;
   }
 }
@@ -129,7 +132,7 @@ function surfaceClassesBySurfaceStds(state={}, action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.surfaceClassesBySurfaceStds;
-    case FILTER_OPTIONS_BY: return filterObject(state, action);
+    case FILTER_OPTIONS_BY_SURFACE_STDS: return filterObject(state, action);
     default: return state;
   }
 }
@@ -138,7 +141,7 @@ function allSurfaceClasses(state=[], action) {
   const { payload } = action;
   switch(action.type) {
     case FETCH_MATERIALS_FROM_DB: return payload.allSurfaceClasses;
-    case FILTER_OPTIONS_BY: return filterList(state, action);
+    case FILTER_OPTIONS_BY_SURFACE_STDS: return filterList(state, action);
     default: return state;
   }
 }
@@ -170,20 +173,20 @@ const materialsReducer = combineReducers({
   gradesByGradeStd, // grades grouped into standards accordingly
   allGradesStds, // array of all grade standards
   allGrades, // collection of all grades from gradesByGradeStds
-  
+
   sizesByProfile, // sizes grouped into profiles accordingly
   allProfiles, // array of all profiles
   allSizes, // collection of all sizes from sizesByProfile
-  
+
   profilesByToleranceStds, // profiles grouped by tolerance standards
   allToleranceStds, // array of all tolerance standards
 
   surfaceStdsByProfiles, // surface standards grouped by profiles
   allSurfaceStds, // array of all surface standards
-  
+
   surfaceClassesBySurfaceStds, // surface classes grouped by surface standards
   allSurfaceClasses, // array of all surface classes
-  
+
   stockMaterialsById, // object of stock materials, fetched according some or all criterias above in row of cell being edited
   stockMaterialsAllIds, // array of all stock materials, fetched according some or all criterias above in row of cell being edited
 });
