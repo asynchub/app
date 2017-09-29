b91c852ccb8252de899a1aabc9df04ba54f853f3
Juri <juri.l@me.com>
2017-09-29 12:18:19 +0300

    Update reducer-materials.js

--- src/reducers/reducer-materials.js
+++ src/reducers/reducer-materials.js
@@ -1,1 +1,22 @@
+import { combineReducers } from 'redux';
 
+import {
+  FETCH_MATERIALS_FROM_DB
+}
+
+const materialsReducer = combineReducers({
+  gradesByGradeStd, // grades grouped into standards accordingly
+  allGradesStds, // array of grade standards
+  allGrades, // collection of all grades from gradesByGradeStds
+  
+  sizesByProfile, // sizes grouped into profiles accordingly
+  allProfiles, // array of profiles
+  allSizes, // collection of all sizes from sizesByProfile
+  
+  
+  
+  toleranceStds,
+  surfaceStds
+})
+
+export default materialsReducer;
