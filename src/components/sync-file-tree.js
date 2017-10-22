43cd84abfa341b60e3b3d279ecf84e50dfd24925
Juri <juri.l@me.com>
2017-10-22 18:19:16 +0300

    Delete sync-file-tree.js

--- src/components/sync-file-tree.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class SyncFileTree extends Component {
-  render() {
-    return (
-      <p>File tree to sync with table</p>
-    );
-  }
-}
