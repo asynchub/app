0a8173ff19ddc10e5e9702648fda01b27deef7cd
Juri <juri.l@me.com>
2017-10-22 18:24:01 +0300

    Delete source-file-tree.js

--- src/components/source-file-tree.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class SourceFileTree extends Component {
-  render() {
-    return (
-      <p>File tree to grab files to table</p>
-    );
-  }
-}
