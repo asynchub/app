648669b9456a78ebec7bd00fefba42e2bf5d7a91
Juri <juri.l@me.com>
2017-10-22 18:18:56 +0300

    Delete login-form.js

--- src/components/login-form.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class LoginForm extends Component {
-  render() {
-    return (
-      <div>Login form to navigate route according to user profile</div>
-    );
-  }
-}
