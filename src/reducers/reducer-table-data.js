3d338e150c625e69c0f0ba20383c2c67794ecd61
Juri <juri.l@me.com>
2017-10-25 22:10:41 +0300

    Delete reducer-table-data.js

--- src/reducers/reducer-table-data.js
+++
@@ -1,9 +0,0 @@
-import { READ_SOURCE_FILE_TREE } from '../actions/index';
-
-export default function TableData(state={}, action) {
-  switch (action.type) {
-    case READ_SOURCE_FILE_TREE:
-      return action.payload;
-  }
-  return state;
-}  
